ed0838fd325bce22ff15af1c226abd73c475ff8c
smoczynskiw <wiktor.smoczynski@netguru.com>
2022-03-04 12:54:45 +0100

    add margin between contact elements

--- src/components/Footer/Text.js
+++ src/components/Footer/Text.js
@@ -1,22 +1,18 @@
 import styled from 'styled-components'
 
-const StyledWrapper = styled.div`
+const StyledText = styled.div`
   font-family: WorkSans-Regular;
   font-size: 13pt;
   letter-spacing: -1px;
-`
-
-const StyledText = styled.span`
-  margin: 0 8px;
-  &:first-of-type {
-    margin-left: 0;
+  & + & {
+    margin-left: 8px;
   }
 `
 
 const Text = ({ text }) => (
-  <StyledWrapper>
-    <StyledText>{text}</StyledText>
-  </StyledWrapper>
+  <StyledText>
+    {text}
+  </StyledText>
 )
 
 export default Text
