2c8276bf143d80aecf8f2777a2e12d3c2fbcfefc
smoczynskiw <wiktor.smoczynski@netguru.com>
2022-03-10 17:57:37 +0100

    refactor

--- src/components/Bio/SeeMyWork/Arrow.js
+++ src/components/Bio/SeeMyWork/Arrow.js
@@ -1,9 +1,20 @@
+import styled from 'styled-components'
 import arrow from '../../../images/arrow.png'
 
+const StyledWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`
+
+const StyledImage = styled.img`
+  height: 55%;
+`
+
 const Arrow = () => (
-  <div style={{ height: '100%', display: 'flex', alignItems: 'center'}}>
-    <img src={arrow} alt='arrow' style={{height: '35%'}}/>
-  </div>
+  <StyledWrapper style={{ height: '100%', display: 'flex', alignItems: 'center'}}>
+    <StyledImage src={arrow} alt='arrow' />
+  </StyledWrapper>
 )
 
 export default Arrow
